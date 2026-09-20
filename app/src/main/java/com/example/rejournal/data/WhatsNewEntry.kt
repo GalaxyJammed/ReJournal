3dbd07d2834e1de9a4d56493cd4170eb7c256c76
GalaxyJam <66970814+GalaxyJamSpeed@users.noreply.github.com>
2026-09-21 00:54:41 +0300

    [Changes/Additions] - You can now add custom emojis to your mood bar instead of a selected default along with 2 custom ones - A select few screens in "More" now show a number right next to the arrow to see how many of each thing you have. - Each card in the app now has a 35% to have a butterfly on it to give the app more soul - Entering the app now provides a splash screen until the app loads instead of a white screen - Added Back Button on search menu - Fixed  bugs

--- app/src/main/java/com/example/rejournal/data/WhatsNewEntry.kt
+++ app/src/main/java/com/example/rejournal/data/WhatsNewEntry.kt
@@ -15,7 +15,7 @@ object WhatsNewEntries {
                 "Added a custom section in the Emojis Appearance area to add your own custom emojis for moods",
                 "A few cards in 'More' now show a number besides them of how much of {x} you have in that card",
                 "Each card has a 35% to have a variation of a miniature butterfly appear in it to give the app more soul",
-                "Entering the app now adds a splash screen of the logo instead of a white screen to help the app load".
+                "Entering the app now adds a splash screen of the logo instead of a white screen to help the app load",
                 "Fixed bug where users could spam the 7 day goal if they had at least 7 days logged",
                 "Added back button on search menu"
 
